86d98fa66f9ea3a001979a7dc295acd0768355b7
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-04-08 20:22:09 +0530

    Implemented a working 2 pass assembler

---
+++ def.h
@@ -0,0 +1,82 @@
+#include<string>
+#include<unordered_map>
+
+enum Register {
+    A,
+    X,
+    L,
+    B, 
+    S, 
+    T, 
+    F, 
+    PC, 
+    SW
+};
+
+enum Format {
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    DATA,
+};
+
+enum DataType {
+    WORD,
+    BYTE,
+    RESB,
+    RESW,
+};
+
+struct Opcode {
+    int code;
+    Format format;
+};
+
+struct Instruction {
+    Opcode opcode;
+    std::string data;
+    Format format;
+    DataType type;
+    int address;
+};
+
+struct formatOne {
+    int opcode;
+};
+
+struct formatTwo {
+    int opcode;
+    Register r1;
+    Register r2;
+};
+
+struct formatThree {
+    int opcode;
+    bool n;
+    bool i;
+    bool x;
+    bool b;
+    bool p;
+    bool e;
+    int displacement;
+};
+
+struct formatFour {
+    int opcode;
+    bool n;
+    bool i;
+    bool x;
+    bool b;
+    bool p;
+    bool e;
+    int address;
+};
+
+struct formatData {
+    int value;
+    bool reserved;
+    bool word;
+};
+
+std::string SPACE = "\n<=====================================================================>\n";
